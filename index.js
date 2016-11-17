21c139812a4c09a671f6d6b38c4be85fb2a15fd2
Josh Black <joshblack@us.ibm.com>
2016-11-17 14:43:14 -0600

    Add content for writing a GraphQL Server in JS

--- index.js
+++ index.js
@@ -2,28 +2,62 @@
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { graphql, buildSchema } = require('graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+} = require('graphql');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
 
-const schema = buildSchema(`
-type Video {
-  id: ID,
-  title: String,
-  duration: Int,
-  watched: Boolean
-}
+const videoType = new GraphQLObjectType({
+  name: 'Video',
+  description: 'A video on Egghead.io',
+  fields: {
+    id: {
+      type: GraphQLID,
+      description: 'The id of the video.',
+    },
+    title: {
+      type: GraphQLString,
+      description: 'The title of the video.',
+    },
+    duration: {
+      type: GraphQLInt,
+      description: 'The duration of the video (in seconds).',
+    },
+    watched: {
+      type: GraphQLBoolean,
+      description: 'Whether or not the viewer has watched the video.',
+    },
+  },
+});
 
-type Query {
-  video: Video
-  videos: [Video]
-}
+const queryType = new GraphQLObjectType({
+  name: 'QueryType',
+  description: 'The root query type.',
+  fields: {
+    video: {
+      type: videoType,
+      resolve: () => new Promise((resolve) => {
+        resolve({
+          id: 'a',
+          title: 'GraphQL',
+          duration: 180,
+          watched: false,
+        });
+      }),
+    },
+  },
+});
 
-type Schema {
-  query: Query
-}
-`);
+const schema = new GraphQLSchema({
+  query: queryType,
+});
 
 const videoA = {
   id: 'a',
@@ -39,20 +73,9 @@ const videoB = {
 };
 const videos = [videoA, videoB];
 
-const resolvers = {
-  video: () => ({
-    id: '1',
-    title: 'Foo',
-    duration: 180,
-    watched: true,
-  }),
-  videos: () => videos,
-};
-
 server.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
-  rootValue: resolvers,
 }));
 
 server.listen(PORT, () => {
