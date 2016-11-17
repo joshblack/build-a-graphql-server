0d9531f6c95094c50d062290cb2d24b0a6c8b506
Josh Black <joshblack@us.ibm.com>
2016-11-17 17:25:16 -0600

    Add content for GraphQLList with GraphQLObject Types

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const graphqlHTTP = require('express-graphql');
 const {
   GraphQLSchema,
   GraphQLObjectType,
+  GraphQLList,
   GraphQLNonNull,
   GraphQLID,
   GraphQLString,
   GraphQLInt,
   GraphQLBoolean,
 } = require('graphql');
-const { getVideoById } = require('./src/data');
+const { getVideoById, getVideos } = require('./src/data');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
@@ -43,6 +44,10 @@ const queryType = new GraphQLObjectType({
   name: 'QueryType',
   description: 'The root query type.',
   fields: {
+    videos: {
+      type: new GraphQLList(videoType),
+      resolve: getVideos,
+    },
     video: {
       type: videoType,
       args: {
--- src/data/index.js
+++ src/data/index.js
@@ -18,5 +18,7 @@ const getVideoById = (id) => new Promise((resolve) => {
 
   resolve(video);
 });
+const getVideos = () => new Promise((resolve) => resolve(videos));
 
 exports.getVideoById = getVideoById;
+exports.getVideos = getVideos;
