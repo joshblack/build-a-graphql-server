90bbf04de52806957be6a8e2e71db0f7d344318d
Josh Black <joshblack@us.ibm.com>
2016-11-20 16:42:02 -0600

    Add content for Relay Connections

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const {
   GraphQLBoolean,
 } = require('graphql');
 const { getVideoById, getVideos, createVideo } = require('./src/data');
-const { globalIdField } = require('graphql-relay');
+const {
+  globalIdField,
+  connectionDefinitions,
+  connectionFromPromisedArray,
+  connectionArgs,
+} = require('graphql-relay');
 const { nodeInterface, nodeField } = require('./src/node');
 
 const PORT = process.env.PORT || 3000;
@@ -41,14 +46,31 @@ const videoType = new GraphQLObjectType({
 });
 exports.videoType = videoType;
 
+const { connectionType: VideoConnection } = connectionDefinitions({
+  nodeType: videoType,
+  connectionFields: () => ({
+    totalCount: {
+      type: GraphQLInt,
+      description: 'A count of the total number of objects in this connection.',
+      resolve: (conn) => {
+        return conn.edges.length;
+      },
+    },
+  }),
+});
+
 const queryType = new GraphQLObjectType({
   name: 'QueryType',
   description: 'The root query type.',
   fields: {
     node: nodeField,
     videos: {
-      type: new GraphQLList(videoType),
-      resolve: getVideos,
+      type: VideoConnection,
+      args: connectionArgs,
+      resolve: (_, args) => connectionFromPromisedArray(
+        getVideos(),
+        args
+      ),
     },
     video: {
       type: videoType,
